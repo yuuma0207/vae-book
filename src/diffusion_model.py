4f56ffc82f2075c85aed123f16acf6ddd4c2a99e
yuuma <yuuma0207@gmail.com>
2025-01-04 22:37:14 +0900

    add file #1

---
+++ src/diffusion_model.py
@@ -0,0 +1,34 @@
+import torch
+
+# 正弦波エンコーディング
+# 整数tに対して、出力次元output_dimのテンソルを返す。
+# iが偶数のとき sin(t / 10000^(i/output_dim))
+# iが奇数のとき cos(t / 10000^(i/output_dim))
+def _pos_encoding(t, output_dim, device='cpu'):
+    '''
+    t: time
+    output_dim: output dimension
+    device: cpu or cuda
+
+    return: tensor of shape (output_dim,)
+    '''
+    D = output_dim
+    v = torch.zeros(D, device=device)
+
+    i = torch.arange(0, D, device=device)
+    div_term = 10000**(i/D)
+
+    v[0::2] = torch.sin(t / div_term[0::2])
+    v[1::2] = torch.cos(t / div_term[1::2])
+    return v
+
+# バッチデータ処理用の関数
+def pos_encoding(ts, output_dim, device='cpu'):
+    batch_size = len(ts)
+    v = torch.zeros(batch_size, output_dim, device=device)
+    for i in range(batch_size):
+        v[i] = _pos_encoding(ts[i], output_dim, device)
+    return v
+
+# テスト
+v = pos_encoding(torch.tensor([1,2,3]),16)
