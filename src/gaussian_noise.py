7f645b8871a1bd8ba943c4120c1c93ae876d3a40
yuuma <yuuma0207@gmail.com>
2025-01-05 22:08:01 +0900

    revised

--- src/gaussian_noise.py
+++ src/gaussian_noise.py
@@ -49,7 +49,9 @@ betas = torch.linspace(beta_start, beta_end, T) # noise schedule
 
 def add_noise(x_0: torch.Tensor, t: int, betas: torch.Tensor):
     T = len(betas)
-    assert t >= 1 and t <= T
+    assert t >= 0 and t <= T
+    if t == 0:
+        return x_0
 
     alphas = 1 - betas
     alpha_bars = torch.cumprod(alphas, dim=0)
@@ -61,11 +63,19 @@ def add_noise(x_0: torch.Tensor, t: int, betas: torch.Tensor):
 
     return x_t
 
-t = 100
-x_t = add_noise(x, t, betas)
+t_list = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+imgs = []
 
-img = reverse_to_img(x_t)
-plt.imshow(img)
-plt.title(f'Noise: {t}')
-plt.axis('off')
+for t in t_list:
+    x_t = add_noise(x, t, betas)
+    img = reverse_to_img(x_t)
+    imgs.append(img)
+
+plt.figure(figsize=(15, 6))
+for i, img in enumerate(imgs):
+    plt.subplot(2, 5, i+1)
+    plt.imshow(img)
+    plt.title(f'Noise: {t_list[i]}')
+    plt.axis('off')
 plt.show()
+
