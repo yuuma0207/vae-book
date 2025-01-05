52873145022863c224a8edb20f40e181b744e3a5
yuuma <yuuma0207@gmail.com>
2025-01-05 13:56:40 +0900

    features 時系列データ用のUnet実装

--- src/diffusion_model.py
+++ src/diffusion_model.py
@@ -37,3 +37,58 @@ def pos_encoding(ts, output_dim, device="cpu"):
 class ConvBlock(nn.Module):
     def __init__(self, in_ch, out_ch, time_embed_dim):
         super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU()
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(time_embed_dim, in_ch),
+            nn.ReLU(),
+            nn.Linear(in_ch, out_ch)
+        )
+    
+    def forward(self, x, v):
+        N, C, _, _ = x.shape
+        v = self.mlp(v) # multi layer perceptron
+        v = v.view(N, C, 1, 1) # (N, C) -> (N, C, 1, 1) reshape
+        y = self.convs(x + v)
+        return y
+
+class UNet(nn.Module):
+    def __init__(self, in_ch, time_embed_dim=100):
+        super().__init__()
+        self.time_embed_dim = time_embed_dim
+
+        self.down1 = ConvBlock(in_ch, 64, time_embed_dim)
+        self.down2 = ConvBlock(64, 128, time_embed_dim)
+        self.bot1 = ConvBlock(128, 256, time_embed_dim)
+        self.up2 = ConvBlock(128 + 256, 128, time_embed_dim)
+        self.up1 = ConvBlock(128 + 64, 64, time_embed_dim)
+        self.out = nn.Conv2d(64, in_ch, 1)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+    
+    def forward(self, x, timestamps):
+        # 正弦波エンコーディング
+        v = pos_encoding(timestamps, self.time_embed_dim, x.device)
+
+        x1 = self.down1(x, v)
+        x = self.maxpool(x1)
+        x2 = self.down(x, v)
+        x = self.maxpool(x2)
+
+        x = self.bot1(x, v)
+        
+        x = self.upsample(x)
+        x = torch.cat([x, x2], dim=1)
+        x = self.up2(x, v)
+        x = self.upsample(x)
+        x = torch.cat([x, x1], dim=1)
+        x = self.up1(x, v)
+        x = self.out(x)
+        return x
